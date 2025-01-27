7b33a46bbdb0c78d64e7a8766aa2a5915148c353
Rayyr <khasaatirayaa@gmail.com>
2025-01-27 18:45:10 +0200

    final_commit

--- x.js
+++ x.js
@@ -1,42 +1,43 @@
 
- var realData,num;
+var realData, num;
 
- document.querySelector("button").onclick=solve;
- 
- 
+document.querySelector("button").onclick = solve;
 
-async function getUsers(){
 
-     const response=await fetch("https://jsonplaceholder.typicode.com/users");
-     realData= await response.json();
-          
-     let user=`<div class="user">
-     <p>${realData[num-1].id}</p>
-     <p>${realData[num-1].name}</p>
-     <p>${realData[num-1].phone}</p>
+
+async function getUsers() {
+
+   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+   realData = await response.json();
+
+   let user = `<div class="user">
+     <p class="id">User ID : ${realData[num - 1].id}</p>
+     <p class="name">User Name : ${realData[num - 1].name}</p>
+     <p class="phone">User Phone : ${realData[num - 1].phone}</p>
      </div>`;
-     document.querySelector("body").innerHTML+= user;
-     console.log(2);
-    }
+   document.querySelector(".cont").innerHTML = user;
 
-  function solve(){
-     
-    
-    console.log(1);
-
-     num=window.prompt("Please write the user number : ( -1 to exit ) ");
-     if(num==-1)
-        window.alert("bi");
-  
-     getUsers();
-     console.log(8);
 }
 
+function solve() {
  
 
-  
+   num = window.prompt("Please write the user number : ( -1 to exit ) ");
+   if (num == -1) {
+      window.alert("Bi");
+      window.close();
+   }
 
- 
+   getUsers();
+
+}
+
+
+
+
+
+
+
 
 
 
